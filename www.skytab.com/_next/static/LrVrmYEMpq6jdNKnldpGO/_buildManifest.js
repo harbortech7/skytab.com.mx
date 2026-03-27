10bdf203c954f2fc96ba240165b8b10748963a58
markinfotech7 <info@harbortechpos.com>
2026-03-27 03:45:35 -0700

    Add files via upload

---
+++ www.skytab.com/_next/static/LrVrmYEMpq6jdNKnldpGO/_buildManifest.js
@@ -0,0 +1,173 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/de6bd803f33ad37e.js"
+  ],
+  "/404": [
+    "static/chunks/466e91db0fe2c385.js"
+  ],
+  "/[...slug]": [
+    "static/chunks/c0b7a99a987964d3.js"
+  ],
+  "/_error": [
+    "static/chunks/9f5cd53429b5ab19.js"
+  ],
+  "/blog": [
+    "static/chunks/46aca4a55df7ca3f.js"
+  ],
+  "/blog/[slug]": [
+    "static/chunks/4af2fb7fef6e3e6a.js"
+  ],
+  "/checkout": [
+    "static/chunks/58b2c6e2caa0bbff.js"
+  ],
+  "/checkout/back-of-house": [
+    "static/chunks/8f465c415c665aa4.js"
+  ],
+  "/checkout/front-of-house": [
+    "static/chunks/71dd09a204bc26bb.js"
+  ],
+  "/checkout/other": [
+    "static/chunks/ffcaf36341702f6d.js"
+  ],
+  "/checkout/pricing": [
+    "static/chunks/bd0d2543f8b5a5c8.js"
+  ],
+  "/checkout/start": [
+    "static/chunks/8d92a486a5a4b009.js"
+  ],
+  "/checkout/summary": [
+    "static/chunks/09b8ed9aca910dad.js"
+  ],
+  "/contact-test": [
+    "static/chunks/fce2830849d7ea0d.js"
+  ],
+  "/cookies": [
+    "static/chunks/f7e181f820180713.js"
+  ],
+  "/digideck": [
+    "static/chunks/7ec4fab578a632e1.js"
+  ],
+  "/enterprise-loyalty": [
+    "static/chunks/eb721ba008294014.js"
+  ],
+  "/impact-referrals": [
+    "static/chunks/195876f0f9df9bc9.js"
+  ],
+  "/online-ordering-worksheet": [
+    "static/chunks/7650d389cfeaf43f.js"
+  ],
+  "/programming": [
+    "static/chunks/7e4fca22063f8908.js"
+  ],
+  "/referral": [
+    "static/chunks/88a5698506279e9a.js"
+  ],
+  "/referral/submit": [
+    "static/chunks/0d60882ebc80ade0.js"
+  ],
+  "/resource-center": [
+    "static/chunks/3efb738a4c9339a4.js"
+  ],
+  "/resource-center/[slug]": [
+    "static/chunks/237952b843ea53fe.js"
+  ],
+  "/resource-center/webinar/[webinarRecordingSlug]": [
+    "static/chunks/0fca95014ab7a77d.js"
+  ],
+  "/sf-contact": [
+    "static/chunks/feab9749cad25679.js"
+  ],
+  "/uk-referral-program": [
+    "static/chunks/7739402544976e66.js"
+  ],
+  "/uk-referral-program/invite": [
+    "static/chunks/0f02571957af9932.js"
+  ],
+  "/uk-referral-program/invite-thankyou": [
+    "static/chunks/6346e7caa1e4d6a1.js"
+  ],
+  "/uk-referral-program/login": [
+    "static/chunks/3f30d948bf691937.js"
+  ],
+  "/uk-referral-program/signup-thankyou": [
+    "static/chunks/8e330419cc175319.js"
+  ],
+  "/uk-referral-program/terms": [
+    "static/chunks/c6125f9a482c1d7c.js"
+  ],
+  "/utm-contact-test": [
+    "static/chunks/2435b3499fb80a09.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/404",
+    "/_app",
+    "/_error",
+    "/api/contact-support-form-submit",
+    "/api/contact-support-image-upload",
+    "/api/digideck_photo",
+    "/api/enterprise-form-access-token",
+    "/api/enterprise-form-submit",
+    "/api/form-checkout-submit",
+    "/api/form-enterpriseloyalty-submit",
+    "/api/form-onlineordering-submit",
+    "/api/form-salesforce-submit",
+    "/api/form-salesforce-submit2",
+    "/api/formsubmit",
+    "/api/formsubmit-up",
+    "/api/global-sf-form-submit",
+    "/api/impact/add-partner-and-link-to-google-sheet",
+    "/api/impact/impact-create-partner",
+    "/api/impact/impact-generate-tracking-link",
+    "/api/impact/impact-get-partner-from-partnerid",
+    "/api/impact/impact-get-partners",
+    "/api/impact/impact-send-referral-submit-email",
+    "/api/impact/impact-send-success-email",
+    "/api/layout-data",
+    "/api/mapbox/get-cities",
+    "/api/no-js-upgrade-submit",
+    "/api/programming-form-submit",
+    "/api/referralrock/add-member",
+    "/api/referralrock/invite",
+    "/api/referralrock/login",
+    "/api/salesforce/referral-post",
+    "/api/spam-detect",
+    "/api/test-email-template",
+    "/api/testing-html",
+    "/blog",
+    "/blog/[slug]",
+    "/checkout",
+    "/checkout/back-of-house",
+    "/checkout/front-of-house",
+    "/checkout/other",
+    "/checkout/pricing",
+    "/checkout/start",
+    "/checkout/summary",
+    "/contact-test",
+    "/cookies",
+    "/digideck",
+    "/enterprise-loyalty",
+    "/impact-referrals",
+    "/online-ordering-worksheet",
+    "/programming",
+    "/referral",
+    "/referral/submit",
+    "/resource-center",
+    "/resource-center/webinar/[webinarRecordingSlug]",
+    "/resource-center/[slug]",
+    "/sf-contact",
+    "/uk-referral-program",
+    "/uk-referral-program/invite",
+    "/uk-referral-program/invite-thankyou",
+    "/uk-referral-program/login",
+    "/uk-referral-program/signup-thankyou",
+    "/uk-referral-program/terms",
+    "/utm-contact-test",
+    "/[...slug]"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
